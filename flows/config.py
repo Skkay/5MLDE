4d87ea65247255fb9d3cb371a75760203bcd4324
Skkay <skkay.thomas@gmail.com>
2023-03-25 15:03:54 +0100

    style: Cleaning workflow.py and config.py

--- flows/config.py
+++ flows/config.py
@@ -1,7 +1,3 @@
-DATA_URL = 'http://51.75.204.199:9002/ogrineRates?sort=desc'
 DROP_COLUMNS = ['id', 'datetime', 'rate']
 TARGET_COLUMN = 'rateTenth'
 TRAIN_TEST_OFFSET = 30
-LOCAL_STORAGE = '/home/skkay/dev/project/5MLDE/exam'
-
-CRON_TRAINING = '0 1 * * 1'  # every monday at 1am
--- flows/workflow.py
+++ flows/workflow.py
@@ -3,7 +3,6 @@ import config
 import requests
 import pandas as pd
 import numpy as np
-import pickle
 import mlflow
 
 from prefect import task, flow
@@ -71,23 +70,6 @@ def evaluate_model(y_true: np.ndarray, y_pred: np.ndarray) -> float:
 
 
 @task()
-def load_pickle(path: str):
-    """Load a pickle file"""
-
-    with open(path, 'rb') as f:
-        loaded_obj = pickle.load(f)
-    return loaded_obj
-
-
-@task()
-def save_pickle(path: str, obj: dict):
-    """Save a pickle file"""
-
-    with open(path, 'wb') as f:
-        pickle.dump(obj, f)
-
-
-@task()
 def extract_X_y(df):
     """Extract X and y from a DataFrame"""
 
@@ -135,7 +117,7 @@ def build_new_data(data):
 
 
 @flow()
-def complete_ml(url: str, save_model: bool = True):
+def complete_ml(url: str):
     """Complete ML pipeline"""
 
     # Download data from URL
@@ -159,10 +141,6 @@ def complete_ml(url: str, save_model: bool = True):
     results = train_and_predict(X_train, y_train, x_test, y_test)
     print(f'MSE: {results["mse"]:.2f}')
 
-    # Save model
-    # if save_model:
-    #     save_pickle(f'{config.LOCAL_STORAGE}/model.pkl', results)
-
     return results
 
 
@@ -174,13 +152,6 @@ def batch_inference(data, model=None):
     df = preprocess_data(df)
     X, _ = extract_X_y(df)
 
-    # if not model:
-    #     model = load_pickle(f'{config.LOCAL_STORAGE}/model.pkl')['model']
-
     predictions = predict(model, X)
 
     return [round(v) for v in predictions]
-
-
-if __name__ == '__main__':
-    complete_ml(config.DATA_URL)
